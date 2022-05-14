86154375935355a20c3a0b698de996d2f61a1b6c
Westin <wcurtis@kvcc.edu>
2022-05-14 11:15:52 -0400

    Set the capacity of the result to the number of headers

--- src/csv_helper.rs
+++ src/csv_helper.rs
@@ -5,7 +5,7 @@ pub fn extract_headers(file_contents: &str) -> Result<Vec<String>, io::Error> {
 
     let mut reader = csv::Reader::from_reader(file_contents.as_bytes());
 
-    let mut result: Vec<String> = vec![];
+    let mut result: Vec<String> = Vec::with_capacity(reader.headers()?.len());
 
     for header in reader.headers()? {
         result.push(header.to_string());
